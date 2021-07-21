71b87e1f9d2ee382cfcf624f96e752e8d587c9ff
Calvin <noname729@gmail.com>
2021-07-22 01:58:49 +0800

    exercise done

--- demo-2/_practical-app/7.php
+++ demo-2/_practical-app/7.php
@@ -1,24 +1,25 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
-    
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
 
-		<aside class="col-xs-4">
+<section class="content">
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+	<aside class="col-xs-4">
+
+		<?php Navigation(); ?>
+
+
+	</aside>
+	<!--SIDEBAR-->
 
 
 	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
 
-	/*  Step 1 - Create a database in PHPmyadmin
+
+
+		<?php
+
+		/*  Step 1 - Create a database in PHPmyadmin
 
 		Step 2 - Create a table like the one from the lecture
 
@@ -27,13 +28,33 @@
 		Step 4 - Connect to Database and read data
 
 */
-	
-	?>
+
+		$connection = mysqli_connect('localhost:8889', 'root', 'root', 'users');
+		if (!$connection) {
+			die('Connection Failed');
+		} else {
+			echo "Well Done";
+		}
+
+		$query = "SELECT * from loginapp WHERE id='4'";
+		$result = mysqli_query($connection, $query);
+		if (!$result) {
+			echo ("No Result" . mysqli_error($connection));
+		}
+
+		while ($row = mysqli_fetch_array($result)) {
+			print_r($row['password']);
+		}
+
+		?>
 
 
 
 
 
-</article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php"; ?>
+
+	</article>
+	<!--MAIN CONTENT-->
+
+	<?php include "includes/footer.php"; ?>
